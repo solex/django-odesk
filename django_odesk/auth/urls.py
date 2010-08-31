7594b6fa0024c1a5c3745deabd466009f72edffc
Oleksiy Solyanyk <samozvanezz@gmail.com>
2010-09-01 01:05:34 +0300

    Fixed login/logout problems

--- django_odesk/auth/urls.py
+++ django_odesk/auth/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django_odesk.auth.views',
     url(r'^login/$', 'login'),
+    url(r'^authorize/$', 'authorize'),
     url(r'^callback/$', 'callback'),
     url(r'^logout/$', 'logout'),
 )
--- django_odesk/auth/views.py
+++ django_odesk/auth/views.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 from django_odesk.core.clients import RequestClient
 
-def login(request):
+def login(request, template_name='registration/odesk_login.html'):
+    return render_to_response(template_name, {}, 
+            context_instance=RequestContext(request))
+
+def authorize(request):
     if 'odesk_api_token' in request.session:
         del request.session['odesk_api_token']
     odesk_client = RequestClient(request)
@@ -19,19 +27,16 @@ def callback(request, redirect_url=None):
         request.session['odesk_auth_user_data'] = auth_user
         redirect_url = request.session.pop('odesk_redirect_url', redirect_url)
         if not redirect_url:
-            redirect_url = getattr(settings, 'LOGIN_REDIRECT_URL', '/')   
+            redirect_url =  '/'   
         return HttpResponseRedirect(redirect_url)
     
     else:
         return HttpResponseRedirect(odesk_client.auth.auth_url())
-    
 
-def logout(request, redirect_url=None):
-    odesk_client = RequestClient(request)
-    odesk_client.auth.revoke_token()
-    if 'odesk_api_token' in request.session:
-        del request.session['odesk_api_token']
-    if not redirect_url:
-        redirect_url = getattr(settings, 'LOGIN_REDIRECT_URL', '/')          
-    return HttpResponseRedirect(redirect_url)
+
+def logout(request):
+    request.session.flush()
+    if hasattr(request, 'odesk_user'):
+        request.odesk_user = AnonymousUser()
+    return HttpResponseRedirect(reverse('django_odesk.auth.views.login'))
     
