0df73bd0e1976c6f35e92d53b388a426c1312a62
Kostia Balitsky <kostia.balitsky@gmail.com>
2011-09-22 21:13:56 +0300

    Fixed a bug with clients

--- django_odesk/core/clients.py
+++ django_odesk/core/clients.py
@@ -6,6 +6,11 @@ from django_odesk.conf import settings
 from django_odesk.auth import ODESK_TOKEN_SESSION_KEY, ENCRYPTION_KEY_NAME
 from django_odesk.auth.encrypt import decrypt_token
 
+try:
+    ENCRYPT_API_TOKEN = settings.ENCRYPT_API_TOKEN
+except AttributeError, e:
+    ENCRYPT_API_TOKEN = False
+
 class DefaultClient(Client):
 
     def __init__(self, api_token=None):
@@ -21,10 +26,14 @@ class DefaultClient(Client):
 class RequestClient(DefaultClient):
 
     def __init__(self, request):
-        encryption_key = request.COOKIES.get(ENCRYPTION_KEY_NAME, None)
-        encrypted_token = request.session.get(ODESK_TOKEN_SESSION_KEY, None)
-        api_token = None
-        if encryption_key and encrypted_token:
-            api_token = decrypt_token(encryption_key, encrypted_token)
+        from_session = request.session.get(ODESK_TOKEN_SESSION_KEY, None)
+        if ENCRYPT_API_TOKEN:
+            encryption_key = request.COOKIES.get(ENCRYPTION_KEY_NAME, None)
+            encrypted_token = from_session
+            api_token = None
+            if encryption_key and encrypted_token:
+                api_token = decrypt_token(encryption_key, encrypted_token)
+        else:
+            api_token = from_session
         super(RequestClient, self).__init__(api_token) 
     
